81415b7e8454f7818080b1310a6e2dbc76e74b45
vitor <vitor.tfreitas@gmail.com>
2016-02-02 07:45:16 -0200

    Improving shell-sort

--- Classical/sorting/shell/shell-sort.sh
+++ Classical/sorting/shell/shell-sort.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
+#!/bin/bash 
 
 items=("$@")
 length=${#items[@]}
 
 major_step_index=$(( length / 2 ))
 
+echo "Major $major_step_index"
 while (( major_step_index > 0 )); do
-	for i in $( seq 0 major_step_index); do 
+	for i in $( seq 0 $major_step_index); do 
 		
-		for j in $( seq $(( i + major_step_index)) length major_step_index ); do
-			current=$items[j]
+		for j in $( seq $(( i + major_step_index)) $length $major_step_index ); do
+			current=${items[$j]}
 			inner_step_index=$j
 				
-			while (( inner_step_index >= major_step_index && items[(( inner_step_index - major_step_index ))] > current )); do
-				items[$inner_step_index]=$items[(( inner_step_index - manjor_step_index ))]
-				inner_step_index=(( inner_step_index - major_step_index ))
+			while (( inner_step_index >= major_step_index && items[$(( inner_step_index - major_step_index ))] > current )); do
+				items[$inner_step_index]=${items[$(( inner_step_index - manjor_step_index ))]}
+				inner_step_index=$(( inner_step_index - major_step_index ))
 			done
 			
 			items[$inner_step_index]=$current	
 		done
 		
-		major_slice_index=(( major_step_index / 2))
+		major_slice_index=$(( major_step_index / 2))
 	done
 done
 
 
-echo ${ items[@] }
+echo "${items[@]}"
