58f5ac710e30a6e73c8246f094bdfee288a66f05
Vitor Barbosa <vitor.tfreitas@gmail.com>
2016-02-16 13:40:13 -0200

    Fixing dequeue RPOP bug

--- Classical/data-structures/dequeue/shell/dequeue.sh
+++ Classical/data-structures/dequeue/shell/dequeue.sh
@@ -2,25 +2,25 @@
 
 items=()
 while true ; do
-	echo "... "
+	printf "> "
 	read -r line
 	args=($line)
 	case ${args[0]} in 
 		"FADD")
 			arg_list=(${args[1]})
-			arg_list+=${items[@]}
-			items=$arg_list
+			arg_list+=(${items[@]})
+			items=(${arg_list[@]})
 			;;
 		"RADD")
 			items+=(${args[1]})
 			;;
 		"FPOP")
 			echo "${items[0]}"
-			items=${items[@]:1}
+			items=(${items[@]:1})
 			;;
-		"RPOP") 
-			echo "${items[-1]}"
-			items=${items[@]:0:$((${#items[@]} - 1))}
+		"RPOP")
+			echo "${items[(( ${#items[@]} - 1))]}"
+			items=(${items[@]:0:$((${#items[@]} - 1))})
 			;;
 	esac
 			
